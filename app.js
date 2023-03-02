784dc1a3e34b56150fa27ee49c12802f408c3bb0
reddforman <edwardhamidov@aol.com>
2023-03-02 14:54:04 -0500

    made updates

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-const sketch = "Hello";
+const container = document.getElementById("container");
+/*
+for (let i = 0; i < 17; i++) {
+    const div = doucment.createElement("div");
+    div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
+    container.appendChild(div);
+}*/
+
+for (let i = 1; i < 257; i++) {
+    const div = document.createElement('div');
+    container.appendChild(div);
+  }
